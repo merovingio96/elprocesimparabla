516b5d8f2f1fb377cbce5f4fb7dd188b01805e94
merovingio96 <abrahamaverroes@gmail.com>
2018-05-18 11:38:19 +0200

    Update google-places.js

--- google-places.js
+++ google-places.js
@@ -184,7 +184,7 @@ Thanks user peledies for the code!!*/
                 console.log(json_response);
                 //Parsear la respuesta JSON de Google para extrar la url que aloja la imagen
                 var url_image = json_response.image.url; //JSON response: image { url {}}  
-                html = html+"<div class='review-item'><div class='review-meta'><span class='review-author-image'><img src=<%=url_image%>/></span>";
+                html = html+"<div class='review-item'><div class='review-meta'><span class='review-author-image'><img src="<%=url_image%>" style="width:104px;height:142px;"></span>";
             }); 
             html = html+"<span class='review-author'>"+reviews[i].author_name+"</span><span class='review-sep'>, </span><span class='review-date'>"+date+"</span></div>"+stars+"<p class='review-text'>"+reviews[i].text+"</p></div>";
           };
